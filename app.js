4ef97041a68fb2370aea60ff5bccda9226321106
Ismail Kuplu <i.kuplu@outlook.com>
2021-03-12 20:20:58 +0100

    Started adding script with an external js file

---
+++ app.js
@@ -0,0 +1,6 @@
+const min = document.getElementsByClassName('min')[0];
+min.textContent = 'Min';
+const sec = document.getElementsByClassName('sec')[0];
+min.textContent = 'Sec';
+const csec = document.getElementsByClassName('csec')[0];
+min.textContent = 'Csec';
