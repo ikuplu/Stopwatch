5fe27867a21fd32ceecde784aa15682fe6733a8b
Ismail Kuplu <i.kuplu@outlook.com>
2021-03-13 20:01:11 +0100

    Added a control board

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ const sec = document.getElementById('sec');
 sec.innerHTML = 'Sec';
 const cs = document.getElementById('cs');
 cs.innerHTML = 'Cs';
+
+const play = document.getElementById('play');
+const pauze = document.getElementById('pauze');
+const stop = document.getElementById('stop');
+play.innerHTML = 'Play';
+pauze.innerHTML = 'Pauze';
+stop.innerHTML = 'Stop';
